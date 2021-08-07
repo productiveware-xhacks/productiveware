dc59d9dbafd0f2324deca3a4793584bea5377c78
object-Object <object@objectobject.ca>
2021-08-07 01:03:04 -0400

    move config to appdata

--- client/productiveware/config.py
+++ client/productiveware/config.py
@@ -1,6 +1,10 @@
 import json
+from pathlib import Path
+import os
 
-_config_file = "../config.json"
+
+_config_dir = os.getenv("APPDATA")+"\productiveware"
+_config_file = _config_dir+"\config.json"
 
 
 def _read():
@@ -12,8 +16,9 @@ def _read():
 
 # might raise an exception if it fails to write to the file
 def _write(config):
+	Path(_config_dir).mkdir(exist_ok=True)
 	with open(_config_file, "w") as f:
-		json.dump(f, config)
+		json.dump(config, f)
 
 
 def get_target_folders():
@@ -22,7 +27,11 @@ def get_target_folders():
 
 def add_target_folder(path):
 	config = _read()
-	target_folders = config.get("target_folders", [])
+	try:
+		target_folders = config["target_folders"]
+	except KeyError:
+		config["target_folders"] = []
+		target_folders = config["target_folders"]
 	target_folders.append(path)
 	_write(config)
 
